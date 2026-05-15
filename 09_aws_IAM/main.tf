e0183c98755212e00bfcc15d968b11495d5cc266
Ramkumar-21-gk <gkramkumarprajapati49@gmail.com>
2026-05-15 13:06:31 +0530

    aws iam policy tf

---
+++ 09_aws_IAM/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+locals {
+  user_data=yamldecode(file("users.yaml")).users
+  user_role_pair=flatten([for user in local.user_data : [for role in user.roles: {
+    username= user.username
+    role= role
+  }]])
+}
+output "name" {
+  value = local.user_role_pair  
+}
+resource "aws_iam_user" "users" {
+  for_each = toset(local.user_data[*].username)
+  name = each.value
+}
+
+resource "aws_iam_user_login_profile" "profile" {
+  for_each = aws_iam_user.users
+  user=each.value.name
+  password_length = 12
+
+  lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
+
+}
+
+# attaching policy
+resource "aws_iam_user_policy_attachment" "main" {
+  for_each = {
+    for pair in local.user_role_pair :
+    "${pair.username}-${pair.role}"=>pair
+  }
+  user = aws_iam_user.users[each.value.username].name
+  policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
+}
